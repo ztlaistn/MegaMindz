b84395d0bbe6d7eced78e025d22bbc5dbf1877e1
djpalumb <djpalumb@users.noreply.github.com>
2021-09-29 21:01:50 -0400

    #44 Modified SQL file
    Modified SQL file to init user_info table.  Also wrote instructions for using it into a textfile.

--- documentation/database/planning/user_cursor.js
+++ documentation/database/planning/user_cursor.js
@@ -3,11 +3,11 @@ const cred = require('./database_cred');
 
 class user_cursor_obj{
     constructor(ID=0){
-        this.ID = ID
+        this.cursor_ID = ID
         this.pool = new Pool(cred)        
     }
     get_cursor_ID(){
-        return {ID: this.ID}
+        return {ID: this.cursor_ID}
     }
 
 }
--- documentation/database/planning/user_info.sql
+++ documentation/database/planning/user_info.sql
@@ -4,34 +4,38 @@
 * TODO: Don't let users input fields that are longer than can be stored in this table
 */
 
-USE <database_name>;
+USE d8k5sebftf48b1;
 
 -- table_name = user_info
 -- Next line will delete table if already exists (so that you can debug without having to delete it every time)
--- DROP TABLE IF EXISTS user_info;
+DROP TABLE IF EXISTS user_info;
 
 CREATE TABLE
 user_info (
 -- It will generate an unique int userID when we add a new row
-	UserID 			INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	UserID 			SERIAL NOT NULL PRIMARY KEY,
 -- Fields required to create account
-	Email 			VARCHAR(100) NOT NULL DEFAULT "",
-	Username 		VARCHAR(50) NOT NULL DEFAULT "",
-	Password		VARCHAR(50) NOT NULL DEFAULT "",
+	Email 			VARCHAR(100) NOT NULL DEFAULT '',
+	Username 		VARCHAR(50) NOT NULL DEFAULT '',
+	Pass			VARCHAR(50) NOT NULL DEFAULT '',
 -- Additional account information (feel free to add more to this, as we add features)
-	FirstName		VARCHAR(30) NOT NULL DEFAULT "",
-	LastName		VARCHAR(30) NOT NULL DEFAULT "",
-	DOB				DATE DEFAULT "0000-00-00",
-	Location		VARCHAR(50) NOT NULL DEFAULT "",
-	Status			VARCHAR(200) NOT NULL DEFAULT "",
-	Skill			VARCHAR(200) NOT NULL DEFAULT "",
+	FirstName		VARCHAR(30) NOT NULL DEFAULT '',
+	LastName		VARCHAR(30) NOT NULL DEFAULT '',
+	DOB				DATE DEFAULT '1900-01-01',
+	Location		VARCHAR(50) NOT NULL DEFAULT '',
+	Status			VARCHAR(200) NOT NULL DEFAULT '',
+	Skills			VARCHAR(200) NOT NULL DEFAULT '',
+	CurrRoom		INT DEFAULT NULL,
 -- Might want to just disable row, rather than delete entry when account is removed
-	Enabled			TINYINT NOT NULL DEFAULT 0,
-	
-	INDEX 	index1 	(Enabled, Username, Password),
-	INDEX 	index2	(Enabled, Lastname, Firstname)
+	Enabled			SMALLINT NOT NULL DEFAULT 0
 );
 
+CREATE INDEX index1 ON user_info(Enabled, Username, Pass);
+
+CREATE INDEX index2 ON user_info(Enabled, Lastname, Firstname);
+
+CREATE INDEX index3 ON user_info(Enabled, CurrRoom, UserID);
+	
 -- Will print table structure when created
-DESCRIBE user_info
+-- DESCRIBE user_info;
 
