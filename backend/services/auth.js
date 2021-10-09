5c9c14be567ccb73e6bb771c01fa3e163f5a744b
malika-k <mkrkodial@gmail.com>
2021-10-08 22:26:53 -0400

    #53 added logic to /register route and auth service

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -1,16 +1,34 @@
 /* service will handle business logic */
-export default AuthService() {
+class AuthService {
   constructor() {
-    //dependencies are added here
+    //dependencies are added here for dependency injection
 
   }
 
-  /*Input:
-    Output:
-    Behavior:
+  /*Input: Object with username, password1, password2, and email
+    Output: Object with successValue [bool], uid [int], and errorMsg [string]
+      SUCCESS: isSuccess=true with valid auth token and uid
+      ERROR: isSuccess=false with error message
   */
-  register() {
+  register(data) {
+    const { username, email, password1, password2 } = data;
+    // check that password 1 and password 2 match
+    if (password1 !== password2) {
+      return {isSuccess: false, errorMsg: "Provided passwords do not match"};
+    }
+    // verify that a user with that email does not already exist
+    // INSERT database method here [from user model]
 
+    // generate salt
+    /*bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+            // store user, store username, email, hash, and salt in db
+            // return uid
+        });
+    });*/
+
+    let tempUid = 29;
+    return {isSuccess: true, uid: tempUid};
   }
 
   /*Input:
@@ -18,7 +36,10 @@ export default AuthService() {
     Behavior:
   */
   logIn() {
-
+    // check if token already exists
+    // check if email is valid
+    // check that encrypted password matches
+    // create token, update db with token, return
   }
 
   /*Input:
@@ -26,6 +47,22 @@ export default AuthService() {
     Behavior:
   */
   logOut() {
+    // might need AUTH header - check this
+    // delete user token from db
+  }
+
+  // create JSON Web Token
+  createToken() {
+
+  }
+
+  validateToken() {
+
+  }
+
+  destroyToken() {
 
   }
 }
+
+export default { AuthService };
