cac13c0176cae66d877a36889ac9ba3e8b3b7bc8
Zackary Laistner <ztlaistn@buffalo.edu>
2021-10-12 11:50:58 -0400

    #49 Implemented Registration Form
    Created jsx and css files for the registration form. Added routing between the login and registration pages

--- frontend/meetngreet/src/components/Login.jsx
+++ frontend/meetngreet/src/components/Login.jsx
@@ -12,30 +12,40 @@ export default class Login extends React.Component {
         };
     }
 
-    check_credentials = () => {
-        if(((document.getElementById("email").validity.valid) && (document.getElementById("password").value.length != 0)) && ((document.getElementById("email").value.includes("@")) && (document.getElementById("email").value.includes(".")))){
-            document.getElementById("login_button").disabled = false;
+    process_login = () => {
+        //implement login handler here
+        console.log("Initiated login");
+        if(((document.getElementById("email").validity.valid) && (document.getElementById("password").value.length !== 0)) && ((document.getElementById("email").value.includes("@")) && (document.getElementById("email").value.includes(".")))){
+            //check database and redirect to home page
         } else {
-            document.getElementById("login_button").disabled = true;
+            window.alert("Error: Missing one or more required fields.")
         }
     }
 
+    check_credentials = () => {
+
+    }
+
+    toRegistration = () => {
+        window.location.href = "registration";
+    };
+
     render() {
         return (
-            <div id="loginform">
+            <div id="login-form">
                 <img src={login_icon} className="login-icon" alt=""/>
-                <div className="logincredentials">
+                <div className="login-credentials">
                     <form>
-                        <label for="email">Email Address</label>
+                        <label for="email" className="text-input-label">Email Address</label>
                         <input type="email" required id="email" name="email" placeholder="Email" onChange={this.check_credentials}/>
-                        <label for="password">Password</label>
+                        <label for="password" className="text-input-label">Password</label>
                         <input type="password" required id="password" name="password" placeholder="Password" onChange={this.check_credentials}/>
-                        <input type="submit" id="login_button" value="Log In" disabled className="button-primary"/>
+                        <input type="button" id="login_button" value="Log In" className="button-primary" onClick={this.process_login}/>
                     </form>
                     <br/>
                     <br/>
                     <header className="register-prompt">Don't have an account?</header>
-                    <input type="submit" value="Register" className="button-secondary"/>
+                    <input type="button" value="Register" className="button-secondary" onClick={this.toRegistration}/>
                 </div>
             </div>
         );
