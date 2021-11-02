5f6b5499ba08379c8b8162f096f529f77196c7b4
ztlaistn <ztlaistn@buffalo.edu>
2021-11-02 02:53:29 -0400

    #85 Implemented Phaser Interface
    Created Phaser interface and applied fixes to properly refresh the home screen on redirects. Also updated Phaser documentation.

--- frontend/meetngreet/src/components/Home.jsx
+++ frontend/meetngreet/src/components/Home.jsx
@@ -28,6 +28,16 @@ export default class Home extends React.Component {
         window.location.href = "user-account";
     };
 
+    toChatroom = () => {
+        if(document.getElementById("code").value === ""){
+            //Send to default chat room
+            window.location.href = "chatroom";
+        } else {
+            //Send to chat room with specified code
+            window.location.href = "chatroom/" + document.getElementById("code").value;
+        }
+    };
+
     render() {
         return (
             <div id="home">
@@ -36,10 +46,8 @@ export default class Home extends React.Component {
                 <b>Welcome back, {this.state.username}</b>
                 <br/>
                 <input type="button" value="User Account" className="button-primary" onClick={this.toUserAccount}/>
-                <Link to ="/chatroom">
-                    <input type="text" required id="message" name="message" placeholder="Enter Room code"/>
-                <input type="button" value="Join Chat Room" className="button-primary"/>
-                </Link>
+                <input type="text" required id="code" name="message" placeholder="Enter Room code"/>
+                <input type="button" value="Join Chat Room" className="button-primary" onClick={this.toChatroom}/>
             </div>
         );
     }
